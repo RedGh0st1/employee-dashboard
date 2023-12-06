712142902068514a9d9211296cfb070dc6b35fdd
Lennie Nurse <lnursej@gmail.com>
2023-12-06 11:46:55 -0500

    fixed typo in employeeProfile

--- src/Pages/EmployeeProfile.jsx
+++ src/Pages/EmployeeProfile.jsx
@@ -1,27 +1,31 @@
-import {useParams} from "react-router-dom"
-import Navbar from "../Components/Navbar"
-import {useEffect, useState} from "react"
-import UpdateEmployeeForm from "../Components/UpdateEmployeeForm"
+import { useParams } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import { useEffect, useState } from "react";
+import UpdateEmployeeForm from "../Components/UpdateEmployeeForm";
 
 export default function EmployeeProfile() {
-  const {id} = useParams()
-  const [employee, setEmployee] = useState([])
+  const { id } = useParams();
+  const [employee, setEmployee] = useState([]);
 
   useEffect(() => {
     async function getEmployeeById() {
-      let request = await fetch(`http://localhost:8000/employee/${id}`)
-      let response = await request.json()
-      console.log(response)
-      if (request.ok) setEmployee(response)
+      let request = await fetch(`http://localhost:8000/employee/${id}`);
+      let response = await request.json();
+      console.log(response);
+      if (request.ok) setEmployee(response);
     }
-    getEmployeeById()
-  }, [])
+    getEmployeeById();
+  }, []);
 
   return (
     <div>
       <Navbar />
       <h1>Employee Profile {id} </h1>
+      <img src={employee.pic} alt="employee pic" />
+      <h1>
+        {employee.first_name} {employee.last_name}
+      </h1>
       <UpdateEmployeeForm />
     </div>
-  )
+  );
 }
